be13af4691942b0dfe540cc9e4b415254cb0a30a
Carlos Hernandez <honchkrow1995@gmail.com>
2026-03-11 23:28:29 -0600

    feat: Implement SEO improvements by adding sitemap generation, a robots.txt file, and enhanced meta tags in the layout.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()],
+  site: 'https://hit-cargo.com',
+  integrations: [preact(), tailwind(), sitemap()],
 });
